37b1019eb18065f058b690b0bafac2b267589787
AArmagost1 <89876520+AArmagost1@users.noreply.github.com>
2022-01-14 20:19:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def consumption():
 
 @app.route("/api/transposed")
 def trans():
-    trans = db.transpose_emissions.find()
+    trans = db.transemissions.find()
     trans = pd.DataFrame(trans)
     trans ['Car Registrations'] = trans ['Car Registrations'].fillna(0)
     trans = trans.rename(columns={'Unnamed: 0':'Year'})
